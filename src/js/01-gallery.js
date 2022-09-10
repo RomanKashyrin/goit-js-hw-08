099cc19e7097ff607d765eb28619e4a20112a3ca
Roman Kashyrin <103502911+Royale16@users.noreply.github.com>
2022-09-10 18:18:34 +0300

    Сделал д/з 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector('.gallery');
+const imageMarkup = createItemsMarkup(galleryItems);
+
+galleryContainerEl.insertAdjacentHTML('beforeend', imageMarkup);
+
+function createItemsMarkup(image) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" />
+            </a>
+        </div>
+        `;
+        })
+        .join("");
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionType: 'alt',
+    captionDelay: 250,
+});
